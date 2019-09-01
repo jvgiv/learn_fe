cda992dd7ac4cf01c2e7eba45cd64c7e59fcf7d2
jvgiv <jvgiv12@gmail.com>
2019-09-01 17:34:51 -0400

    Auth0 Shell complete

--- src/react-auth0-wrapper.js
+++ src/react-auth0-wrapper.js
@@ -24,10 +24,67 @@ export const Auth0Provider = ({
             const auth0FromHook = await createAuth0Client(initOptions);
             setAuth0(auth0FromHook)
 
-            if (window.location.search.includes("code=") {
+            if (window.location.search.includes("code=")) {
                 const { appState } = await auth0FromHook.handleRedirectCallback();
                 onRedirectCallback(appState)
-            })
+            }
+            
+            const isAuthenticated = await auth0FromHook.isAuthenticated()
+            
+            setIsAuthenticated(isAuthenticated)
+            
+            if (isAuthenticated) {
+                const user = await auth0FromHook.getUser()
+                setUser(user)
+            }
+            
+            setLoading(false)
         }
-    })
+        
+        initAuth0()
+    }, [])
+    
+    const loginWithPopup = async (params ={}) => {
+        setPopupOpen(true);
+        try {
+            await auth0Client.loginWithPopup(params);
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setPopupOpen(false)
+        }
+        const user = await auth0Client.getUser()
+        setUser(user)
+        setIsAuthenticated(true)
+    }
+
+    const handleRedirectCallback = async () => {
+        setLoading(true)
+        await auth0Client.handleRedirectCallback()
+        const user = await auth0Client.getUser()
+        setLoading(false);
+        setIsAuthenticated(true);
+        setUser(user)
+    }
+
+    return (
+        <Auth0Context.Provider
+            value={{
+                isAuthenticated,
+                user,
+                loading,
+                popupOpen,
+                loginWithPopup,
+                handleRedirectCallback,
+                getIdTokenClaims: (...p) => auth0Client.getIdTokenClaims(...p),
+                loginWithRedirect: (...p) => auth0Client.loginWithRedirect(...p),
+                getTokenSilently: (...p) => auth0Client.getTokenSilently(...p),
+                getTokenWithPopup: (...p) => auth0Client.getTokenWithPopup(...p),
+                logout: (...p) => auth0Client.logout(...p)
+            }}
+        >
+            {children}
+        </Auth0Context.Provider>
+    )
+
 }
