b4f6855a2d3b42aca2855085cf26e19d5f6bd8a5
jvgiv <jvgiv12@gmail.com>
2019-10-30 19:01:52 -0400

    Styling added for lessons, working on profile query

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,42 @@
 import React from 'react'
 import { useAuth0 } from '../react-auth0-wrapper'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_USER = gql`
+    query {
+        getUserById(id: ID!) {
+            first_name
+        }
+    }
+`
 
 const Profile = () => {
-    const { loading, user } = useAuth0()
+    // const { loading, user } = useAuth0()
 
-    if (loading || !user) {
-        return (
-            <div>
-                Loading...
-            </div>
-        )
-    }
+    // if (loading || !user) {
+    //     return (
+    //         <div>
+    //             Loading...
+    //         </div>
+    //     )
+    // }
 
-    console.log(user)
+    const { loading, error, data } = useQuery(GET_USER)
+    if (loading) return 'Loading...'
+    if (error) return `Error! ${error.message}`
+
+    // console.log(user)
     return (
-        <>
-            <img src={user.picture} alt="Profile" />
+        <div>
+
+
+            {/* <img src={user.picture} alt="Profile" />
 
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-            <code>{JSON.stringify(user, null, 2)}</code>
-        </>
+            <code>{JSON.stringify(user, null, 2)}</code> */}
+        </div>
     )
 }
 
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,8 +3,17 @@ import React from 'react'
 const Skill = (props) => {
     console.log(props.data, "COCK")
     
+
+    const skillBox = {
+        border: '1px solid black',
+        borderRadius: '5px',
+        margin: '0 auto',
+        width: '30%',
+        marginBottom: '10px'
+    }
+
     return (
-        <div>
+        <div style={skillBox}>
             <p>{props.data.skill}</p> 
             <p>{props.data.skill_subset}</p> 
         </div>
