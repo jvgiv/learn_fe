ad1a7fee553e6f775a2ad546e236d12064345970
jvgiv <jvgiv12@gmail.com>
2019-10-29 22:01:56 -0400

    Data managed properly off of first Apollo call

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Skill = (props) => {
-    console.log(props.skillData.getSkills, "COCK")
+    console.log(props.data, "COCK")
+    
     return (
         <div>
-            
+            <p>{props.data.skill}</p> 
+            <p>{props.data.skill_subset}</p> 
         </div>
     )
 }
--- src/components/SkillSearch.js
+++ src/components/SkillSearch.js
@@ -3,6 +3,7 @@ import { useAuth0 } from '../react-auth0-wrapper'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
 import Skill from './Skill.js'
+import Loader from 'react-loader-spinner'
 
 const GET_SKILLS = gql`
     query {
@@ -18,18 +19,21 @@ const GET_SKILLS = gql`
 `
 
 export default function LessonSearch() {
-    const [skills, setSkills] = useState({})
-    const { loading, error, data, onCompleted } = useQuery(GET_SKILLS)
-        
-    //     , {
-    //     onCompleted: data => {
-    //         setSkills(data)
-    //     }
-    // });
+    useEffect(() => {
+        console.log('boner')
+    })
+    const [skills, setSkills] = useState([])
+    const { loading, error, data } = useQuery(GET_SKILLS, {
+        onCompleted: data => {
+            setSkills(data)
+        }
+    });
     if (loading) return 'Loading...'
     if (error) return `Error! ${error.message}`
+
+
     
-    console.log(data, 'skills', skills)
+    console.log(data.getSkills, 'skills', skills.getSkills)
 
     // state = {
         
@@ -47,9 +51,10 @@ export default function LessonSearch() {
     return (
         <div>
             <h1>What do you want to learn today?</h1>
+            
             {data.getSkills.map(skill => {
                 return (
-                    <Skill skillData={data}/>
+                    <Skill data={skill}/>
                 )
             })}
         </div>
